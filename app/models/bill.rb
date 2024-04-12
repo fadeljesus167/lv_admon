d54da912f6a0ea814cf0709003407912a6f1e6ee
Fadel <infmain@DESKTOP-IM9F0RJ>
2024-04-12 16:51:34 -0400

    delete delivered field and adding bill_type field to bills talbe

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -3,6 +3,7 @@ class Bill < ApplicationRecord
   validates :bill_reference, presence: true, uniqueness: true
 
   enum status: { processing: 0, canceled: 1, completed: 2 }
+  enum bill_type: { transfer: 0, credit: 1, cash: 2}
 
   belongs_to :fee
   belongs_to :payment
--- db/migrate/20240130154150_create_bills.rb
+++ db/migrate/20240130154150_create_bills.rb
@@ -3,11 +3,11 @@ class CreateBills < ActiveRecord::Migration[7.1]
     create_table :bills do |t|
       t.references :payment, null: false, foreign_key: true, default: 1
       t.integer :status, null: false, default: 0
-      t.boolean :delivered, null: false, default: false
       t.date :bill_date, null: false
       t.date :delivered_date
       t.string :bill_reference, null: false
       t.string :bill_description, null: false
+      t.integer :bill_type, null: false, default: 0
 
       t.timestamps
     end
