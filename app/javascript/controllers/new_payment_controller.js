f0feed2b8330caf0745fff746ec349badab020ef
Fadel <fadeljesus@gmail.com>
2024-05-13 15:10:51 -0400

    adding modal to new payment and some style to bills

--- app/javascript/controllers/new_payment_controller.js
+++ app/javascript/controllers/new_payment_controller.js
@@ -3,6 +3,21 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="new-payment"
 export default class extends Controller {
   connect() {
-    console.log("Hello World")
+    //console.log("Hello World")
+    this.modal = new bootstrap.Modal('#myModal', {})
+  }
+
+  open() {
+    // if (!this.modal.isOpened){
+    //   this.modal.show()
+    // }
+
+    this.modal.show()
+  }
+
+  close(event) {
+    if (event.detail.success) {
+      this.modal.hide()
+    }
   }
 }
