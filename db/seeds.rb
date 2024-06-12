d4f3fe310fb4c5f9890fa14f519fc10ffe7e9aa3
Fadel <infmain@DESKTOP-IM9F0RJ>
2024-06-12 16:11:54 -0400

    seeds for deploy in render

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-User.create(username: 'admin', password: '1234')
+User.create(username: 'admin', password: '1234', role: :admin)
+User.create(username: 'accountant', password: '1234', role: :accountant)
+User.create(username: 'biller', password: '1234', role: :biller)
+
+SchoolTerm.create(start_date: '01/08/2023', end_date: '20/07/2024')
